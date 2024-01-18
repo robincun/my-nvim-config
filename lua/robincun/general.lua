917bf60b21dd56ceea54c5092ddc998dadfa20ee
robincun <esaurobin098@gmail.com>
2024-01-19 00:41:15 +0100

    added virtual end of line

--- lua/robincun/general.lua
+++ lua/robincun/general.lua
@@ -1,7 +1,8 @@
 -- hier sind einige generelle nvim einstellungen
 
 vim.wo.number = true
-
+-- :set virtualedit=onemore
+vim.cmd([[set virtualedit=onemore]])
 vim.opt.termguicolors = true
 -- Set color scheme to gruvbox
 vim.cmd([[colorscheme tokyonight-night]])
